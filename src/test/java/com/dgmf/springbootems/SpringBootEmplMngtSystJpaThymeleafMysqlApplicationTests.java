44e3a7ff939dd6d6dfc2591dfecdb467f2ec4f3a
FranckDodo91 <devlogappdgmf@gmail.com>
2023-06-23 08:18:43 +0200

    Project initialization

---
+++ src/test/java/com/dgmf/springbootems/SpringBootEmplMngtSystJpaThymeleafMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgmf.springbootems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEmplMngtSystJpaThymeleafMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
